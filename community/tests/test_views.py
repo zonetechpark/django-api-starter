48d20f58e0e66d667e1a190a05553ef863038264
Daniel Ale <danielale9291@gmail.com>
2020-08-11 20:10:44 +0100

    TDD update and delete

--- community/tests/test_views.py
+++ community/tests/test_views.py
@@ -71,3 +71,49 @@ class CreateNewPuppyTest(TestCase):
     def test_create_invalid_payload(self):
         response = client.post(reverse('puppy'), data=json.dumps(self.invalid_payload), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateSinglePuppyTest(TestCase):
+    """Test module for updating an existing puppy record"""
+    
+    def setUp(self):
+        self.casper = Puppy.objects.create(name='Casper', age=4, breed='Bull Dog', color='Black')
+        self.muffin = Puppy.objects.create(name='Muffin', age=3, breed='Gradane', color='Brown')
+        self.valid_payload = {
+            "name": "Muffin",
+            "age": 2,
+            "breed": "Pamerion",
+            "color": "White"
+        }
+        self.invalid_payload = {
+            "name": "",
+            "age": 4,
+            "breed": "Pamerion",
+            "color": "White"
+        }
+    
+    def test_valid_update_puppy(self):
+        response = client.put(reverse('puppy-detail', kwargs={'pk': self.muffin.pk}),
+                              data=json.dumps(self.valid_payload), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_invalid_update_puppy(self):
+        response = client.put(reverse('puppy-detail', kwargs={'pk': self.muffin.pk}),
+                              data=json.dumps(self.invalid_payload), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteSinglePuppyTest(TestCase):
+    """Test module for deleting an existing puppy record"""
+
+    def setUp(self):
+        self.casper = Puppy.objects.create(name='Casper', age=4, breed='Bull Dog', color='Black')
+        self.muffin = Puppy.objects.create(name='Muffin', age=3, breed='Gradane', color='Brown')
+
+    def test_valid_delete_puppy(self):
+        response = client.delete(reverse('puppy-detail', kwargs={'pk': self.muffin.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_invalid_delete_puppy(self):
+        response = client.delete(reverse('puppy-detail', kwargs={'pk': uuid.uuid4()}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- community/views.py
+++ community/views.py
@@ -18,9 +18,14 @@ def get_delete_update_puppy(request, pk):
         serializer = PuppySerializer(puppy)
         return Response(serializer.data)
     elif request.method == 'DELETE':
-        return Response({})
+        puppy.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     elif request.method == 'PUT':
-        return Response({})
+        serializer = PuppySerializer(puppy, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'POST'])
