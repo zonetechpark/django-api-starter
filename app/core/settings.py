7afbd4659c6eb19d5ae6f0fe59458f6c9fec15e1
Daniel Ale <danielale9291@gmail.com>
2020-08-16 08:58:15 +0100

    Update

--- app/core/settings.py
+++ app/core/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'yc_zvg#_m*$3rw1_ekj_4%n!rw65ev#h*0(7-x^4k8q38@m^wf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',
+                 '164.90.219.136', 'api.dev.incubator.ng']
 
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
